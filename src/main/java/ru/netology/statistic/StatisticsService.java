98cbc2f154a9274465a1c5ce9b7e2bd1aae8c6a0
Denwin <biscooter@mail.ru>
2023-11-22 18:12:16 +0600

    init

---
+++ src/main/java/ru/netology/statistic/StatisticsService.java
@@ -0,0 +1,23 @@
+package ru.netology.statistic;
+
+public class StatisticsService {
+        public long findMax(long[] incomes) {
+            long currentMax = incomes[0];
+            for (long income = 0; income < incomes.length; income++) {
+                if (currentMax < income) {
+                    currentMax = income;
+                }
+            }
+            return currentMax;
+        }
+
+        public long findMin(long[] incomes) {
+            long currentMin = incomes[0];
+            for (long income : incomes) {
+                if (currentMin > income) {
+                    currentMin = income;
+                }
+            }
+            return currentMin;
+        }
+}
